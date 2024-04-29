7f34bb65a634ee550cd31aaa18f255ac3415e33b
aniketmm <aniketmukherjee5@gmail.com>
2024-04-29 17:55:03 +0530

    changes

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,37 +1,84 @@
-import React from 'react'
-import './HomePage.css'
-import dark_mode from "../../Assets/dark_mode.png"
-import bell from "../../Assets/bell.png"
-import info from "../../Assets/info.png"
-import search from "../../Assets/search.png"
+import React from "react";
+import "./HomePage.css";
+import dark_mode from "../../Assets/dark_mode.png";
+import bell from "../../Assets/bell.png";
+import info from "../../Assets/info.png";
+import search from "../../Assets/search.png";
+import chart from "../../Assets/chart.png";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 
 const HomePage = () => {
   return (
-    <div className='homepage-container'>
-        <div className='homepage-header'>
-            <div className='homepage-user-info'>
-                <p>aniketm@truad.co</p>
-                <h4>Hi Aniket</h4>
-            </div>
-            <div className='homepage-searchbar'>
-                <div className='homepage-searchbar-container'>
-                    <div className='searchbar'>
-                        <input type='text' placeholder='Search'></input>
-                        <img src={search}></img>
-                    </div>
-                    <div className='homepage-searchbar-icons'>
-                        <img src={bell}></img>
-                        <img src={dark_mode}></img>
-                        <img src={info}></img>
-                        <div className='homepage-profile'>
-                            <img src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D'></img>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div className="homepage-container">
+      <div className="homepage-header">
+        <div className="homepage-user-info">
+          <p>aniketm@truad.co</p>
+          <h4>Hi Aniket</h4>
         </div>
+        <div className="homepage-searchbar">
+          <div className="homepage-searchbar-container">
+            <div className="searchbar">
+              <input type="text" placeholder="Search"></input>
+              <img src={search}></img>
+            </div>
+            <div className="homepage-searchbar-icons">
+              <img src={bell}></img>
+              <img src={dark_mode}></img>
+              <img src={info}></img>
+              <div className="homepage-profile">
+                <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="homepage-info-container">
+        <div className="homepage-info">
+          <div className="homepage-infobox">
+            <div className="homepage-infobox-icon">
+              <BarChartIcon />
+            </div>
+            <div className="homepage-infobox-info">
+              <p>Total Invoices</p>
+              <h4>121</h4>
+            </div>
+          </div>
+        </div>
+        <div className="homepage-info">
+          <div className="homepage-infobox">
+            <div className="homepage-infobox-icon">
+              <CurrencyRupeeIcon />
+            </div>
+            <div className="homepage-infobox-info">
+              <p>Payment Pending</p>
+              <h4>32</h4>
+            </div>
+          </div>
+        </div>
+        <div className="homepage-info">
+          <div className="homepage-infobox">
+            <div className="homepage-infobox-icon">
+              <CurrencyRupeeIcon />
+            </div>
+            <div className="homepage-infobox-info">
+              <p>Cleared Invoices</p>
+              <h4>89</h4>
+            </div>
+          </div>
+        </div>
+        <div className="homepage-ticket-btn">
+          <p>Have an Issue? Raise a ticket</p>
+        </div>
+        <div className="homepage-upload-btn">
+          <p>Upload New Materials</p>
+        </div>
+      </div>
+      <div className="homepage-clips">
+        <div className=""></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
