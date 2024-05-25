61d40c8ecd4e9bbe91ce3d2cfcf313de2e356ef8
Aniket Mukherjee <aniketmukherjee5@gmail.com>
2024-05-25 20:49:36 +0530

    aston band

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -4,7 +4,6 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="vertical-bar-loader">
-      {/* <span className="loading-text">Loading...</span> */}
       <div className="bar"></div>
       <div className="bar"></div>
       <div className="bar"></div>
